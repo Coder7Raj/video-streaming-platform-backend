a13d8738d625a818200474ec482f0a604de809e0
Coder7Raj <alwaysraj733@gmail.com>
2025-11-25 00:59:30 +0600

    user auth controller added

---
+++ controllers/authController.js
@@ -0,0 +1,34 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+
+exports.signup = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = new User({ name, email, password });
+    await user.save();
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    res.status(201).json({ token, user });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ error: "User not found" });
+
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) return res.status(400).json({ error: "Invalid password" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    res.json({ token, user });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
