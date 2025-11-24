e3c1e4dde6147f4b27512362ce3647e0934f0ce8
Coder7Raj <alwaysraj733@gmail.com>
2025-11-25 01:28:15 +0600

    updated server

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
 mongoose
-  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
-  .catch((err) => console.log(err));
+  .catch((err) => console.error("MongoDB connection error:", err));
